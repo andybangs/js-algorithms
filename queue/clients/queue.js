273f3bcb0c4eff1e67850ed27329c2375cf6fed1
Andy Bangs <andrew.yuta@gmail.com>
2015-09-16 00:19:48 -0700

    Create directory for each algorithm and data structure

--- queue/clients/queue.js
+++ queue/clients/queue.js
@@ -1,6 +1,6 @@
-import QueueWM from '../QueueWM';
+import Queue from '../Queue';
 
-const queue = new QueueWM();
+const queue = new Queue();
 
 queue.enqueue('red');
 queue.enqueue('green');
@@ -9,9 +9,9 @@ queue.enqueue('blue');
 console.log('size()', queue.size());        // 3
 console.log('isEmpty()', queue.isEmpty());  // false
 
-console.log('dequeue()', queue.dequeue());        // 'red'
-console.log('dequeue()', queue.dequeue());        // 'green'
-console.log('dequeue()', queue.dequeue());        // 'blue'
+console.log('dequeue()', queue.dequeue());  // 'red'
+console.log('dequeue()', queue.dequeue());  // 'green'
+console.log('dequeue()', queue.dequeue());  // 'blue'
 
 console.log('size()', queue.size());        // 0
 console.log('isEmpty()', queue.isEmpty());  // true
--- queue/clients/queueWM.js
+++ queue/clients/queueWM.js
@@ -1,6 +1,6 @@
-import Queue from '../Queue';
+import QueueWM from '../QueueWM';
 
-const queue = new Queue();
+const queue = new QueueWM();
 
 queue.enqueue('red');
 queue.enqueue('green');
@@ -9,9 +9,9 @@ queue.enqueue('blue');
 console.log('size()', queue.size());        // 3
 console.log('isEmpty()', queue.isEmpty());  // false
 
-console.log('dequeue()', queue.dequeue());        // 'red'
-console.log('dequeue()', queue.dequeue());        // 'green'
-console.log('dequeue()', queue.dequeue());        // 'blue'
+console.log('dequeue()', queue.dequeue());  // 'red'
+console.log('dequeue()', queue.dequeue());  // 'green'
+console.log('dequeue()', queue.dequeue());  // 'blue'
 
 console.log('size()', queue.size());        // 0
 console.log('isEmpty()', queue.isEmpty());  // true
