e93c0763e56b57123a00a6451a728f1a10b6d8e2
Andy Bangs <andrew.yuta@gmail.com>
2015-10-15 16:29:23 -0700

    fix insertion sort

--- insertion-sort/insertionSort.js
+++ insertion-sort/insertionSort.js
@@ -1,28 +1,17 @@
-import swap from '../helpers/swap';
+// Based on Nicholas Zakas' insertion sort implementation
+// https://github.com/nzakas/computer-science-in-javascript
 
 export default function (items) {
   for (let i = 0, len = items.length; i < len; i++) {
-    for (let j = i; j > 0 && items[j] < items[j - 1]; j--) {
-      swap(items, j, j- 1);
+    const val = items[i];
+    let j;  // needs to be accessable outside of inner loop
+
+    for (j = i - 1; j > -1 && items[j] > val; j--) {
+      items[j + 1] = items[j];
     }
+
+    items[j + 1] = val;
   }
 
   return items;
 }
-
-// The alternate implementation below does not use the swap helper function.
-
-// export default function (items) {
-//   for (let i = 0, len = items.length; i < len; i++) {
-//     const val = items[i];
-//     let j;
-//
-//     for (j = i - 1; j > -1 && items[j] > val; j--) {
-//       items[j + 1] = items[j];
-//     }
-//
-//     items[j + 1] = val;
-//   }
-//
-//   return items;
-// }
